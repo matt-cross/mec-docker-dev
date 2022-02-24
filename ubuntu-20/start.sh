82e3c65bbf6cd845740815ef513957c7b2a606f5
Matthew Cross <matt.cross@gmail.com>
2022-02-24 17:55:38 -0500

    Create the "authorized_keys" file in the script directory explicitly (in case the script was run from a different directory).

--- ubuntu-20/start.sh
+++ ubuntu-20/start.sh
@@ -3,7 +3,7 @@
 THISDIR=$(dirname $0)
 . ${THISDIR}/conf.sh
 
-cat ${HOME}/.ssh/id*.pub > authorized_keys
+cat ${HOME}/.ssh/id*.pub > ${THISDIR}/authorized_keys
 
 docker build -t ${IMAGE_NAME} ${THISDIR}
 docker run -p ${SSH_PORT}:22 --mount "src=${HOME}/sources,dst=/home/${USERNAME}/sources,type=bind" -d --name ${IMAGE_NAME} ${IMAGE_NAME}
