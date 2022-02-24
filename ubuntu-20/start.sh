b9200402e3576a21ef456539b4c99ef83dd6cbc6
Matt Cross <mcross@irobot.com>
2022-02-24 15:13:35 -0500

    Update to support X redirection over ssh, and make ssh key generation automatic from current user's config (instead of having the public key in this repo).

--- ubuntu-20/start.sh
+++ ubuntu-20/start.sh
@@ -3,6 +3,8 @@
 THISDIR=$(dirname $0)
 . ${THISDIR}/conf.sh
 
+cat ${HOME}/.ssh/id*.pub > authorized_keys
+
 docker build -t ${IMAGE_NAME} ${THISDIR}
 docker run -p ${SSH_PORT}:22 --mount "src=${HOME}/sources,dst=/home/${USERNAME}/sources,type=bind" -d --name ${IMAGE_NAME} ${IMAGE_NAME}
 
